613e10ae73736416ddb99928603669e2ade59dde
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-02-25 14:35:53 +0200

    css table ground

---
+++ frontend/main.js
@@ -0,0 +1,14 @@
+class Card {
+    constructor(suit, rank, isJoker) {
+      this.suit = suit;
+      this.rank = rank;
+      this.isJoker = isJoker;
+    }
+  }
+  class Deck{
+      constructor(){
+          this.cards = [
+              
+          ]
+      }
+  }
